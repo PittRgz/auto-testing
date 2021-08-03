cf7de8d7537002a23755c3fd0bc5ea0393792999
Pedro Rodriguez <pedrorodriguez@MacBook-Pro-de-Pedro.local>
2021-08-03 13:56:28 -0500

    Fixing PyLint and Flake8 Errors

--- app/tests/test_examples.py
+++ app/tests/test_examples.py
@@ -1,7 +1,18 @@
-import pytest
+"""
+This is a simple tests file
+"""
+
 
 def test_sum():
-    assert 4 + 3 == 7
+    """Testing a sum of two numbers"""
+    val_1 = 4
+    val_2 = 3
+    res = 7
+    assert val_1 + val_2 == res
+
 
 def test_strings_equal():
-    assert 'equal' == 'equal'
+    """Testing that two strings are equal"""
+    string_1 = 'string'
+    string_2 = 'string'
+    assert string_1 == string_2
